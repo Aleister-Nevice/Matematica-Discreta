d5e23e40fafd3f3132941bf824e15581ee3ef070
Aleister-Nevice <luizsci42@gmail.com>
2016-07-19 10:26:28 -0300

    Comentários adicionados

--- divisores.py
+++ divisores.py
@@ -2,15 +2,28 @@
 #-*-coding: utf-8-*-
 """
 Programa para auxiliar a encontrar as soluçãos da questão 08 do capítulo 1
+
+Obs: No Linux, é preciso dizer ao shell que o texto abaixo é um programa em python, para isso precisamos informar na primeira linha do arquivo o path do Python: usr/bin/python
+Já o #-*-coding:utf-8-*- serve para que o interpretador reconheça os caracteres acentuados como à, á, ê, etc.
 """
 
+# Recebe um valor fornecido pelo usuario e o atribui à variável n
 n = input("Digite um número para saber quantos divisores tem: ")
+
+# Cria uma lista vazia chamada divisores
 divisores = []
 
+# Cria uma variável x cujo valor irá variar de 1 a n-1 (o valor fornecido pelo usuário. ex: para n = 5, x irá variar de 1 a 4)
+# O trecho de código dentro do for irá se repetir enquanto x for menor que n
 for x in range (1, n):
+    # Se o resto da divisão entre n e x for igual a 0 (Ex: para n=1 ocorrerá o seguinte: 5/1, 5/2, 5/3, 5/4 sendo o denominador o valor de x 
     if (n % x == 0):
+        # Se o resto da divisão for igual a 0, n é divisível por x. Adicione x à lista divisores
         divisores.append(x)
 
+# A função len() serve para "medir" o tamanho de uma lista. Por exemplo, para x = 6, temos 3 divisores: 1, 2 e 3
 tamanho = len(divisores)
 
+# Dentro do print podemos concatenar variáveis usando % , a letra que o segue identifica o tipo de variável
+# %d para inteiros , %s para strings, %2.f para float (sendo 2 o número de casas decimais a serem exibidas após a vírgula)
 print("O número %d tem %d divisores" %(n, tamanho))
--- perfeito.py
+++ perfeito.py
@@ -11,30 +11,54 @@ As questões podem ser resolvidas ao informar ao programa um número inferior a 
 
 """
 
+# Esstou definindo uma função chamada verifica perfeito que recebe uma variável n como argumento. Uma função é um código que será executado apenas quando chamado
 def verifica_perfeito(n):
     
+    # Cria uma lista vazia
     divisores = []
+    # Cria uma variável soma e lhe atribui o valor 0
     soma = 0
 
+    # O trecho de código dentro do for será executado enquanto x < n. Os valores de x irão variar de 1 a n-1
     for x in range(1,n):
-        if (n%x == 0):
+        # Se o resto da divisão entre n e x for = 0
+        if (n % x == 0):
+            # então adicione x à lista de divisores
             divisores.append(x)
-
+    # A variável x irá percorrer a lista divisores. Ou seja: x assumirá os valores contidos na lista
     for x in (divisores):
+        # Aqui a variável soma terá seu próprio valor somado à variável x. (x por sua vez tem o valor de algum número da lista divisores)
+        # Ex: Para x = 2 temos:
+        # 0 = 0 + 2 -> soma = 2
+        # Supondo que o próximo valor de x seja 4:
+        # 2 = 2 + 4 -> soma = 6
         soma = soma + x
-     
+    
+    # Se o total da soma for igual ao número n fornecido como argumento para a função
     if (soma == n):
         print ("O número %d é perfeito" %n)
-        #print ("Seus divisores são: " + divisores)
+        print ("Seus divisores são: " + divisores)
         
+        # Toda função retorna algo. Neste caso, a função verifica_perfeito retorna uma variável booleana cujo valor é True
         return True
 
+# Um programa Python pode ser executado de duas formas:
+# I - Sendo importado por outro programa Python
+# II - Sendo executado diretamente. 
+# A linha abaixo diz que o trecho de código dentro do if deve ser executado apenas se este programa estiver sendo executado diretamente e não quando for importado por outro programa
 if __name__ == "__main__":
     
-    x = input("Entre com o valor inicial de x: ")
+    # Recebe um valor digitado pelo usuário e o atribui à variável v
+    v = input("Entre com o valor inicial de v: ")
     
+    # O trecho de código dentro do While irá se repetir infinitamente, pois a condição dada (True) é sempre verdadeira
     while True:
-        if (verifica_perfeito(x) == True):
+        # Chama a função verifica_perfeito() e envia como argumento a variável v cujo valor foi informado pelo usuário.
+        # Se a função retornar True
+        if (verifica_perfeito(v) == True):
+            # Pare a repetição while
             break
+        # Senão
         else:
-            x = x + 1
+            # Pegue o valor informado pelo usuário, some a 1 e repita o programa
+            v = v + 1
